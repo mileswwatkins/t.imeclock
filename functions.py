1882af5461749c1d21121454de877f5e9b6e306a
Miles Watkins <mileswwatkins@gmail.com>
2013-09-28 19:37:21 -0400

    Condensed tests into one Python file, added another test.

--- functions.py
+++ functions.py
@@ -1,12 +1,25 @@
+from datetime import datetime
+
 class User(object):
-    def __init__(self, username, first_proj):
+    def __init__(self, username):
         self.username = username
         self.proj_dict = {}
         self.proj_list = []
+        self.open_proj = None
 
-    def new_proj(self, proj_name, start_time):
-        pass
+    def new_proj(self, proj_name):
+        self.open_proj = proj_name
 
 
 class Project(object):
-    pass
+    def __init__(self, proj_name):
+        self.proj_name = proj_name
+        self.start_time = None
+        self.end_time = None
+        self.total_time = 0.
+
+    def start_proj(self):
+        pass
+
+    def end_proj(self):
+        pass
--- tests/user_tests.py
+++ tests/user_tests.py
@@ -2,14 +2,16 @@ import unittest
 import os
 import sys
 sys.path.append(os.path.join('..', 'src'))
-from functions import User, Project
+from functions import User
 
 class UserTests(unittest.TestCase):
 
     def setUp(self):
-        self.Miles = User(username="Miles", first_proj="ELE")
+        self.Miles = User(username="Miles")
         
     def test_naming_of_user(self):
         self.assertEqual(self.Miles.username, "Miles")
 
-    def test_
+    def test_open_project_indicator(self):
+        self.Miles.new_proj(proj_name="ELE")
+        self.assertEqual(self.Miles.open_proj, "ELE")
