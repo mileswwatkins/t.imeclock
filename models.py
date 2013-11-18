9020212d22ed3c9d38f634d308f3ae215147a41b
Miles Watkins <mileswwatkins@gmail.com>
2013-11-18 00:53:31 -0500

    Started structuring the website, and added at least dummy pages for all planned URLs.

--- models.py
+++ models.py
@@ -10,9 +10,11 @@ class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    password = Column(String, nullable=False)
     projects = relationship('Project')
 
-    def __init__(self, name):
+    def __init__(self, name, email, password):
         self.name = name
 
     def __repr__(self):
--- routes.py
+++ routes.py
@@ -1,4 +1,9 @@
 from flask import Flask, render_template, redirect, url_for, g, request
+
+from flask_wtf import Form
+from wtforms import TextField, PasswordField
+from wtforms.validators import DataRequired, Email, Length
+
 from contextlib import closing
 
 from sqlalchemy import create_engine
@@ -12,6 +17,7 @@ from models import User, Project
 # Set application constants, and create application
 DATABASE = '/tmp/timeclock.db'
 DEBUG = True
+SECRET_KEY = 'whateverrrr'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -23,33 +29,57 @@ def shutdown_session(exception=None):
     session.remove()
 
 
-# Add a new user based on the URL, and print their name out
-@app.route('/new_user/<username>')
-def new_user(username):
-    user = User(username)
-    session.add(user)
-    session.commit()
-    return "User added: {}".format(username)
+# Create a login and registration form
+class RegisterForm(Form):
+    name = TextField('Username', validators=[
+            Length(min=1, max=32, message="Maximum username length is 32")])
+    email = TextField('Email', validators=[
+            Email(message="A valid email address, please")])
+    password = PasswordField('Password', validators=[
+            Length(min=1, max=32, message="Maximum password length is 32")])
+    confirm_password = PasswordField('Confirm Password', validators=[
+            Length(min=1, max=32, message="Maximum password length is 32")])
 
-# Print list of all users
-@app.route('/user_list')
-def user_list():
-    if User.query.all() is not []:
-        all_users = ''
-        for user in User.query.all():
-            all_users = all_users + user.name + ' '
-        return all_users
-    else:
-        return "No users in database"
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        # Log the user in here with Flask-Login library
+        return redirect('/user_list')
+    return render_template('login.html', form=form)
 
-@app.route('/new_user_form', methods=['POST', 'GET'])
-def new_user_form():
-    if request.method == 'POST':
-        user = User(request.form['username'])
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = User(name=form.name.data, email=form.email.data,
+                password=form.password.data)
         session.add(user)
         session.commit()
-    return render_template('new_user_form.html')
+         # Log the user in here with Flask-Login library
+        return redirect('/user_list')
+    return render_template('register.html', form=form)
 
+@app.route('/current', methods=['POST', 'GET'])
+def current():
+    pass
+
+@app.route('/today')
+def today():
+    pass
+
+@app.route('/history', methods=['POST', 'GET'])
+def history():
+    pass
+
+# For administrative and development purposes, print a list of all users
+@app.route('/user_list')
+def user_list():
+    return render_template('user_list.html', users=User.query.all())
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run()
