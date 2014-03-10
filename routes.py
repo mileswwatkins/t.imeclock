ded62f62ed5fbc5eb4b8d318d129cfe64233a976
Miles Watkins <mileswwatkins@gmail.com>
2014-03-09 23:59:29 -0400

    Enhanced text representation of datetimes for history view.

--- routes.py
+++ routes.py
@@ -46,16 +46,24 @@ def before_request():
 
 # Utility function to convert duration objects into human-readable form
 def duration_to_plain_english(duration):
-    duration_text = ""
-
     duration_mins, duration_secs = divmod(duration.seconds, 60)
     duration_hours, duration_mins = divmod(duration_mins, 60)
 
+    duration_text = ""
     if duration.days > 0:
-        duration_text += str(duration.days) + " days, "
+        if duration.days == 1:
+            duration_text += "1 day, "
+        else:
+            duration_text += str(duration.days) + " days, "
     if duration_hours > 0:
-        duration_text += str(duration_hours) + " hours, "
-    duration_text += str(duration_mins) + " minutes"
+        if duration_hours == 1:
+            duration_text += "1 hour, "
+        else:
+            duration_text += str(duration_hours) + " hours, "
+    if duration_mins == 1:
+        duration_text += "1 minute"
+    else:
+        duration_text += str(duration_mins) + " minutes"
     return duration_text
 
 
